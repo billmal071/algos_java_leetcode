41a809ddd697f0457b2b8923c2402213870d1e5a
WMalachy <theshinobi@pop-os.localdomain>
2023-08-28 00:26:50 +0100

    feat: leetcode two pointer questions

--- src/IsPalindrome.java
+++ src/IsPalindrome.java
@@ -4,4 +4,16 @@ public class IsPalindrome {
         String result = new StringBuilder(formattedString).reverse().toString();
         return formattedString.equals(result);
     }
+
+    public boolean useTwoPointer(String s) {
+//        String formattedString = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int i = 0;
+        int j = s.length() - 1;
+        while (i <= j) {
+            if (s.toLowerCase().charAt(i++) != s.toLowerCase().charAt(j--)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/LongestConsecutive.java
+++ src/LongestConsecutive.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class LongestConsecutive {
+import java.util.*;
+
+public class LongestConsecutive {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) return 0;
+        Set<Integer> hs = new HashSet<>();
+        for (int num : nums) hs.add(num);
+        int longest = 1;
+        for (int num : nums) {
+            if (!hs.contains(num - 1)) {
+                int count = 1;
+                while (hs.contains(num + 1)) {
+                    num++;
+                    count++;
+                }
+                longest = Math.max(longest, count);
+            }
+            if (longest > nums.length / 2) break;
+        }
+        return longest;
+    }
 }
