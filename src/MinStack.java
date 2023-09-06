4e896ee37966de4f8a2672793b71d85804a69859
Williams <billmal071@gmail.com>
2023-09-06 21:38:01 +0100

    feat: get the min from the stack

--- src/MinStack.java
+++ src/MinStack.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class MinStack {
+import java.util.Stack;
+
+public class MinStack {
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack;
+
+    public MinStack() {
+        this.stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        this.min = Math.min(val, min);
+        this.stack.push(val);
+    }
+
+    public void pop() {
+        if (min == this.stack.pop()) {
+            min = this.stack.get(1);
+        } else {
+            this.stack.pop();
+        }
+    }
+
+    public int top() {
+        return this.stack.peek();
+    }
+
+    public int getMin() {
+        min = this.stack.stream().min(Integer::compareTo).orElse(0);
+        return min;
+    }
 }
