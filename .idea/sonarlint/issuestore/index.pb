9cc2a7204e4c277fc8303a9cd8a7de7923d72fc8
Williams <billmal071@gmail.com>
2023-09-12 15:13:27 +0100

    feat: leetcode stack reverse polish notation solution

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,7 @@ E
 src/MergeStrings.java,b/7/b7e9260197afb09c95c3c09f8584a55798710d9b
 C
 src/Palindrome.java,8/a/8a362de877f2d67611b5f44595cc41973069aa54
+B
+src/Replicate.java,3/5/35a49c078be774fbb6c1fef41f444408ddd2cbb3
+D
+src/DingleMouse.java,6/1/61572791c9155174f2abb5a2255afe5cf9a60d06
--- src/EvalRPN.java
+++ src/EvalRPN.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class EvalRPN {
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
+public class EvalRPN {
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        List<String> operators = List.of("+", "-", "*", "/");
+        for (String token : tokens) {
+            if (operators.contains(token)) {
+                int right = stack.pop();
+                int left = stack.pop();
+                switch (token) {
+                    case "+":
+                        stack.push(left + right);
+                        break;
+                    case "-":
+                        stack.push(left - right);
+                        break;
+                    case "*":
+                        stack.push(left * right);
+                        break;
+                    case "/":
+                        stack.push(left / right);
+                        break;
+                }
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        int result = 0;
+        for (String token : tokens) {
+            if (token.equals("+")) {
+                result = stack.pop() + stack.pop();
+                stack.push(result);
+            } else if (token.equals("-")) {
+                result = -stack.pop() + stack.pop();
+                stack.push(result);
+            } else if (token.equals("*")) {
+                result = stack.pop() * stack.pop();
+                stack.push(result);
+            } else if (token.equals("/")) {
+                int divisor = stack.pop();
+                result = stack.pop() / divisor;
+                stack.push(result);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
 }
