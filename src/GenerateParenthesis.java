270777410221a6a6c814a418b27c67fc15c3d068
Williams <billmal071@gmail.com>
2023-09-18 13:11:08 +0100

    Solution from neetcode for generate parenthesis

---
+++ src/GenerateParenthesis.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+public class GenerateParenthesis {
+    Stack<Character> stack = new Stack<>();
+    List<String> res = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        backtrack(0, 0, n);
+        return res;
+    }
+
+    private void backtrack(int openN, int closedN, int n) {
+        if (openN == closedN && closedN == n) {
+            Iterator<Character> vale = stack.iterator();
+            String temp = "";
+            while (vale.hasNext()) {
+                temp = temp + vale.next();
+            }
+            res.add(temp);
+        }
+        if (openN < n) {
+            stack.push('(');
+            backtrack(openN + 1, closedN, n);
+            stack.pop();
+        }
+        if (closedN < openN) {
+            stack.push(')');
+            backtrack(openN, closedN + 1, n);
+            stack.pop();
+        }
+    }
+}
