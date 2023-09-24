33cc655098b255b8184d3461e8a6497b99aeec0f
Williams <billmal071@gmail.com>
2023-09-24 20:44:35 +0100

    feat: search matrix

---
+++ src/SearchMatrix.java
@@ -0,0 +1,9 @@
+public class SearchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int outerLength = matrix.length;
+        int innerLength = matrix[0].length;
+        // you just need to know the biggest and the smallest for the arrays
+        
+        return false;
+    }
+}
